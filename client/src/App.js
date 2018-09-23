462da279c1185e03d59971a3af6083b25f6e2f38
John Sawiris <johnedwardsawiris@gmail.com>
2018-09-22 19:05:31 -0700

    set up Redux boilerplate, created store, reducers, actions folder with some initial content

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,8 @@
 import React, { Component } from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
+import { Provider } from "react-redux";
+import store from "./redux/store";
+// Styles
 import "./App.css";
 
 // Components
@@ -12,19 +15,21 @@ import Login from "./components/auth/Login";
 class App extends Component {
   render() {
     return (
-      <Router>
-        <div className="App">
-          <Navbar />
-          <main className="main-content">
-            <Route exact path="/" component={Landing} />
-            <div className="container">
-              <Route exact path="/register" component={Register} />
-              <Route exact path="/login" component={Login} />
-            </div>
-          </main>
-          <Footer />
-        </div>
-      </Router>
+      <Provider store={store}>
+        <Router>
+          <div className="App">
+            <Navbar />
+            <main className="main-content">
+              <Route exact path="/" component={Landing} />
+              <div className="container">
+                <Route exact path="/register" component={Register} />
+                <Route exact path="/login" component={Login} />
+              </div>
+            </main>
+            <Footer />
+          </div>
+        </Router>
+      </Provider>
     );
   }
 }
